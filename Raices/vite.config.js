2a7974927f3ec331f1ede127633a2ea2b1aac47c
VinkoRobi <Vinkolino178@gmail.com>
2024-07-08 21:56:01 -0500

    Deleted Million

--- Raices/vite.config.js
+++ Raices/vite.config.js
@@ -1,11 +1,7 @@
-
-import MillionLint from '@million/lint';
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-const _plugins = [react()];
-_plugins.unshift(MillionLint.vite())
 export default defineConfig({
-  plugins: _plugins
-});
+  plugins: [react()],
+})
