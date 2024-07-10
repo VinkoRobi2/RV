9b2a9b8300c65ddfad54738e7fc6dd1f41a0efec
VinkoRobi <Vinkolino178@gmail.com>
2024-07-10 18:21:35 -0500

    Ansley

--- Raices/vite.config.js
+++ Raices/vite.config.js
@@ -1,8 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import million from 'million/compiler'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import million from 'million/compiler';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [million.vite({auto:true}),react()],
-})
+  plugins: [
+    million.vite({ auto: true }),
+    react()
+  ],
+  build: {
+    outDir: 'dist', // Directorio de salida para los archivos construidos
+    assetsDir: '', // Directorio para activos estáticos (opcional)
+    manifest: true, // Generar un archivo manifest.json (opcional)
+    rollupOptions: {
+      // Configuración de Rollup, por ejemplo:
+      input: {
+        main: 'src/main.jsx' // Ruta al archivo principal de tu aplicación
+      }
+    }
+  }
+});
